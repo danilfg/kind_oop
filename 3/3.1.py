2e813303141c6d3259dfa4a7aecd209a802c380e
Daniil Nikolaev mac <danilfg@yahoo.com>
2024-06-09 18:01:32 +0300

    add 3.2

--- 3/3.1.py
+++ 3/3.1.py
@@ -1,80 +1,80 @@
 # 8
-import time
+# import time
 
-class GeyserClassic:
-    MAX_DATE_FILTER = 100
+# class GeyserClassic:
+#     MAX_DATE_FILTER = 100
 
-    def __init__(self):
-        self.filter_class = ('Mechanical', 'Aragon', 'Calcium')
-        self.filters = {(1, self.filter_class[0]): None, 
-                        (2, self.filter_class[1]): None, 
-                        (3, self.filter_class[2]): None}
+#     def __init__(self):
+#         self.filter_class = ('Mechanical', 'Aragon', 'Calcium')
+#         self.filters = {(1, self.filter_class[0]): None, 
+#                         (2, self.filter_class[1]): None, 
+#                         (3, self.filter_class[2]): None}
 
-    def add_filter(self, slot_num, filter):
-        key = (slot_num, filter.__class__.__name__)
-        if key in self.filters and not self.filters[key]:
-            self.filters[key] = filter
+#     def add_filter(self, slot_num, filter):
+#         key = (slot_num, filter.__class__.__name__)
+#         if key in self.filters and not self.filters[key]:
+#             self.filters[key] = filter
 
-    def remove_filter(self, slot_num):
-        if type(slot_num) == int and 1 <= slot_num <= 2:
-            key  = (slot_num, self.filter_class[slot_num - 1])
-            if key in self.filters:
-                self.filters[key] = None
+#     def remove_filter(self, slot_num):
+#         if type(slot_num) == int and 1 <= slot_num <= 2:
+#             key  = (slot_num, self.filter_class[slot_num - 1])
+#             if key in self.filters:
+#                 self.filters[key] = None
 
-    def get_filters(self):
-        return tuple(self.filters.values())
+#     def get_filters(self):
+#         return tuple(self.filters.values())
 
-    def water_on(self):
-        end = time.time()
-        for f in self.filters.values():
-                if f is None:
-                    return False
-                start = f.date
-                if end - start > self.MAX_DATE_FILTER:
-                    return False
+#     def water_on(self):
+#         end = time.time()
+#         for f in self.filters.values():
+#                 if f is None:
+#                     return False
+#                 start = f.date
+#                 if end - start > self.MAX_DATE_FILTER:
+#                     return False
                 
-        return True
+#         return True
     
 
-class Mechanical:
-    def __init__(self, date):
-        self.date = date
+# class Mechanical:
+#     def __init__(self, date):
+#         self.date = date
 
-    def __setattr__(self, key, value):
-        if key == 'date' and key in self.__dict__:
-            return
-        super().__setattr__(key, value) # чтобы создать элемент, если еще не создали
+#     def __setattr__(self, key, value):
+#         if key == 'date' and key in self.__dict__:
+#             return
+#         super().__setattr__(key, value) # чтобы создать элемент, если еще не создали
 
     
-class Aragon:
-    def __init__(self, date):
-        self.date = date
+# class Aragon:
+#     def __init__(self, date):
+#         self.date = date
 
-    def __setattr__(self, key, value):
-        if key == 'date' and key in self.__dict__:
-            return
-        super().__setattr__(key, value) # чтобы создать элемент, если еще не создали
-
-
-class Calcium:
-    def __init__(self, date):
-        self.date = date
-
-    def __setattr__(self, key, value):
-        if key == 'date' and key in self.__dict__:
-            return
-        super().__setattr__(key, value) # чтобы создать элемент, если еще не создали
+#     def __setattr__(self, key, value):
+#         if key == 'date' and key in self.__dict__:
+#             return
+#         super().__setattr__(key, value) # чтобы создать элемент, если еще не создали
 
 
-my_water = GeyserClassic()
-my_water.add_filter(1, Mechanical(time.time()))
-my_water.add_filter(2, Aragon(time.time()))
-w = my_water.water_on() # False
-my_water.add_filter(3, Calcium(time.time()))
-w = my_water.water_on() # True
-f1, f2, f3 = my_water.get_filters()  # f1, f2, f3 - ссылки на соответствующие объекты классов фильтров
-my_water.add_filter(3, Calcium(time.time())) # повторное добавление в занятый слот невозможно
-my_water.add_filter(2, Calcium(time.time())) # добавление в "чужой" слот также невозможно
+# class Calcium:
+#     def __init__(self, date):
+#         self.date = date
+
+#     def __setattr__(self, key, value):
+#         if key == 'date' and key in self.__dict__:
+#             return
+#         super().__setattr__(key, value) # чтобы создать элемент, если еще не создали
+
+
+# my_water = GeyserClassic()
+# my_water.add_filter(1, Mechanical(time.time()))
+# my_water.add_filter(2, Aragon(time.time()))
+# w = my_water.water_on() # False
+# my_water.add_filter(3, Calcium(time.time()))
+# w = my_water.water_on() # True
+# f1, f2, f3 = my_water.get_filters()  # f1, f2, f3 - ссылки на соответствующие объекты классов фильтров
+# my_water.add_filter(3, Calcium(time.time())) # повторное добавление в занятый слот невозможно
+# my_water.add_filter(2, Calcium(time.time())) # добавление в "чужой" слот также невозможно
 
 
 # # 0
