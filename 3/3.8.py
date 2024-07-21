e67fe771d05b2ed535fcd22a91d3663f9cf8b3a4
Daniil Nikolaev mac <danilfg@yahoo.com>
2024-07-21 13:55:59 +0300

    add 3.8 2

--- 3/3.8.py
+++ 3/3.8.py
@@ -1,6 +1,36 @@
-# 2
+# 3
 
 
+# # 2
+# class Track:
+#     def __init__(self, start_x, start_y):
+#         self.track = []
+#         # self.track.append([(start_x, start_y), 0])
+        
+#     def add_point(self, x, y, speed):
+#         self.track.append([(x, y), speed])
+        
+#     def __getitem__(self, item):
+#         if item > len(self.track) - 1 and isinstance(item, int):
+#             raise IndexError('некорректный индекс')
+#         return self.track[item][0], self.track[item][1]
+     
+#     def __setitem__(self, key, value):
+#         if key > len(self.track) - 1 and isinstance(key, int):
+#             raise IndexError('некорректный индекс')
+#         self.track[key][1] = value
+
+
+# tr = Track(10, -5.4)
+# tr.add_point(20, 0, 100) # первый линейный сегмент: indx = 0
+# tr.add_point(50, -20, 80) # второй линейный сегмент: indx = 1
+# tr.add_point(63.45, 1.24, 60.34) # третий линейный сегмент: indx = 2
+
+# tr[2] = 60
+# c, s = tr[2]
+# print(c, s)
+
+# res = tr[3] # IndexError
 # # 1
 # class Record:
 #     def __init__(self, *args, **kwargs):
